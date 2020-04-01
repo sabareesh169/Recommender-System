5bad689d40db9b28e00f317aab8d7181193cf874
Sabareesh Mamidipaka <sabareesh169@gmail.com>
2020-04-01 08:43:22 -0700

    Update ECom.py
    Added few comments.

--- ECom.py
+++ ECom.py
@@ -11,7 +11,13 @@ from collections import defaultdict
 from surpECom import surpECom
 
 class ECom:
-
+    """
+    Takes the file name and loads the data. Does preprocessing and convert the dataframe into 
+    surprise dataset (framework for recommender systems).
+    
+    param
+    filename: name of the file. Currently loading only partial amount of data.
+    """
     itemID_to_name = {}
     name_to_itemID = {}
 
@@ -28,13 +34,17 @@ class ECom:
         else:
             raise ValueError('Invalid file format.  Please specify "excel", "csv", "tsv", or "txt".')
         
-        self._preprocess(order_col='order_number', min_order_size=10)
+        ''' Perform preprocessing including dropping orders with less than 5 items to get better recommendations'''
+        self._preprocess(order_col='order_number', min_order_size=5)
 
+        ''' Get a dictionary of most popular items'''
         self.rankings = self.getPopularityRanks()
         
+        '''Convert to surprise dataset framework for further work'''
         self.surpData = surpECom(self.df, self.rankings)
 
     def getOrderPurchases(self, order):
+        '''Looks up all the purchases for a particular order ID'''
         orderPurchases = []
         hitOrder = False
         for index, row in self.df.iterrows():
@@ -49,11 +59,14 @@ class ECom:
         return orderPurchases
 
     def getPopularityRanks(self):
+        ''' Dictionary to record each purchase of the item'''
         purchases = defaultdict(int)
         rankings = defaultdict(int)
         for index, row in self.df.iterrows():
             itemID = int(row[1])
             purchases[itemID] += 1
+        
+        ''' Sort the dictionary based on the number of purchases of the item'''
         rank = 1
         for itemID, purchaseCount in sorted(purchases.items(), key=lambda x: x[1], reverse=True):
             rankings[itemID] = rank
@@ -61,12 +74,14 @@ class ECom:
         return rankings
         
     def getItemName(self, itemID):
+        ''' Get item name from item ID'''
         if itemID in self.itemID_to_name:
             return self.itemID_to_name[itemID]
         else:
             return ""
         
     def getItemID(self, itemName):
+        ''' Get item name from item ID'''
         if itemName in self.name_to_itemID:
             return self.name_to_itemID[itemName]
         else:
@@ -85,7 +100,9 @@ class ECom:
         self.df = self.df[self.df.groupby('order_number').order_number.transform(len) >= min_order_size]
         
     def _makeItemNameDict(self):
+        ''' creates the dictionary with item ID as key and name as value'''
         self.itemID_to_name = dict(zip(self.df['itemID'], self.df['l3']))
     
     def _makeItemIdDict(self):
+        ''' creates the dictionary with item name as key and ID as value'''
         self.name_to_itemID = dict(zip(self.df['l3'], self.df['itemID']))
